212211d1ea042f215cca101ab4ecd997a0f3e072
unknown <miyashaheen1995@gmail.com>
2020-11-09 21:20:11 +0530

    Added:First name validation code using regex

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration Validation System!"
+
+#check first name (Starting with capital and has min 3 characters)
+read -p "Enter First Name : " user_name
+username_pattern="^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$"
+if [[ $user_name =~ $username_pattern ]]
+then
+        echo "Valid First name!"
+else
+        echo "Invalid First name!"
+fi
